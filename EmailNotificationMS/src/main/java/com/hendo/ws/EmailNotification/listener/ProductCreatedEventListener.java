c67815c39f99e031dffa342a6dbf938411d954b4
Hendawy <abdohendawy99@gmail.com>
2025-07-11 19:15:32 +0300

    two projects

---
+++ EmailNotificationMS/src/main/java/com/hendo/ws/EmailNotification/listener/ProductCreatedEventListener.java
@@ -0,0 +1,18 @@
+package com.hendo.ws.EmailNotification.listener;
+
+import com.hendo.ws.EmailNotification.dto.event.ProductCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = "${spring.kafka.topic.product-created-events}")
+@Slf4j
+public class ProductCreatedEventListener {
+
+    @KafkaHandler
+    public void handle(ProductCreatedEvent productCreatedEvent) {
+        log.info("received an event {} ", productCreatedEvent.getTitle());
+    }
+}
---
+++ EmailNotificationMS/src/test/java/com/hendo/ws/EmailNotification/EmailNotificationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendo.ws.EmailNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
